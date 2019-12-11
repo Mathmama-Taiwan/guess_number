a2ea29ad122cd0281eb85aa3b705aee7ac9e2eff
Charlie Li <charliehahaha@gmail.com>
2019-12-11 21:43:00 +0800

    add guest number

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,16 @@
 import random
+
 print('歡迎來到猜數字遊戲')
 num = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	guessnum = int(input('請輸入1~100的整數: '))
 	if guessnum == num:
 		print('終於猜對了！')
 		break
 	elif guessnum > num:
-		print('比',guessnum,'小')
+		print('比', guessnum, '小')
 	elif guessnum < num:
-		print('比',guessnum,'大')
+		print('比', guessnum, '大')
+print('你總共猜了', count, '次')
